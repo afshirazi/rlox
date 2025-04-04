0a41f6c0ed42c2fae02d776819d808f92217b11b
Ali Shirazi <afashirazi@proton.me>
2025-04-04 17:11:50 +0300

    stage changes before clippy

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -1,4 +1,11 @@
-use std::{env, fs, io, process::exit};
+use std::{
+    env, fs,
+    io::{self, Write},
+    process::exit,
+};
+mod tokens;
+
+use tokens::Token;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,6 +27,17 @@ fn run_file(file_name: &str) -> () {
 
 fn run_prompt() -> () {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("Your input was: {input}");
+    loop {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        if input.trim_end() == "exit" {
+            return;
+        }
+        println!("Your input was: {input}");
+    }
+}
+
+fn report(line: u32, where_in_line: &str, message: &str) -> () {
+    //TODO: make better
+    eprintln!("[Line {line}]: Error at {where_in_line}. {message}.");
 }
