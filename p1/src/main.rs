129d18d4ef74d69702e817bf03a2cd694ccf9936
Ali Shirazi <afashirazi@proton.me>
2025-04-10 00:08:56 +0300

    stupid changes

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -2,37 +2,59 @@ use std::{
     env, fs,
     io::{self},
 };
-mod tokens;
 mod scanner;
+mod tokens;
+
+struct Lox {
+    hasError: bool,
+}
+
+impl Lox {
+    fn init(args: Vec<String>) -> () {
+        let mut lox = Lox { hasError: false };
+
+        match args.len() {
+            1 => lox.run_prompt(),
+            2 => lox.run_file(&args[1]),
+            _ => eprintln!("Usage: rlox <file>"),
+        };
+    }
+
+    fn run_file(&self, file_name: &str) -> () {
+        let file = fs::read_to_string(file_name).unwrap();
+        Self::run(&file);
+    }
+
+    fn run_prompt(&mut self) {
+        let mut input = String::new();
+        loop {
+            if self.hasError {
+                self.hasError = false;
+            }
+
+            input.clear();
+            io::stdin().read_line(&mut input).unwrap();
+            if input.trim_end() == "exit" {
+                return;
+            }
+
+            Self::run(&input);
+        }
+    }
+
+    fn run(source: &str) {
+        // do something
+    }
+
+    fn report(&mut self, line: u32, loc_in_line: u32, chars_in_line: &str, message: &str) {
+        //TODO: make better
+        eprintln!("[Line {line}:{loc_in_line}] Error at {chars_in_line}: {message}.");
+        self.hasError = true;
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        1 => run_prompt(),
-        2 => run_file(&args[1]),
-        _ => eprintln!("Usage: rlox <file>"),
-    };
-}
-
-fn run_file(file_name: &str) {
-    let file = fs::read_to_string(file_name).unwrap();
-    println!("{file}");
-}
-
-fn run_prompt() {
-    let mut input = String::new();
-    loop {
-        input.clear();
-        io::stdin().read_line(&mut input).unwrap();
-        if input.trim_end() == "exit" {
-            return;
-        }
-        println!("Your input was: {input}");
-    }
-}
-
-fn report(line: u32, loc_in_line: u32, chars_in_line: &str, message: &str) {
-    //TODO: make better
-    eprintln!("[Line {line}:{loc_in_line}] Error at {chars_in_line}: {message}.");
+    Lox::init(args);
 }
--- p1/src/scanner.rs
+++ p1/src/scanner.rs
@@ -1,6 +1,6 @@
-use crate::{report, tokens::{Literal, Token, TokenType}};
+use crate::{tokens::{Literal, Token, TokenType}, Lox};
 
-pub struct Scanner {
+pub struct Scanner<'a> {
     source: String,
     tokens: Vec<Token>,
 
@@ -11,16 +11,18 @@ pub struct Scanner {
     start: u32,
     current: u32,
     line: u32,
+    report: &'a dyn Fn(u32, u32, &str, &str) -> () // this feels stupid
 }
 
-impl Scanner {
-    pub fn new(source: String) -> Self {
+impl <'a> Scanner <'a> {
+    pub fn new(source: String, report_fn: &'a dyn Fn(u32, u32, &str, &str) -> ()) -> Self {
         Self {
             source: source,
             tokens: vec![],
             start: 0,
             current: 0,
             line: 0,
+            report: report_fn
         }
     }
 
@@ -29,56 +31,133 @@ impl Scanner {
         &self.tokens
     }
 
-    pub fn scanTokens(&mut self) -> () {
-        while (!self.isAtEnd()) {
+    pub fn scan_tokens(&mut self) -> () {
+        while (!self.is_at_end()) {
             self.start = self.current;
 
-            self.scanToken();
+            self.scan_token();
         }
 
-        self.tokens.push(Token::new(
-            TokenType::Eof,
-            "".to_owned(),
-            None,
-            self.line,
-        ));
+        self.tokens
+            .push(Token::new(TokenType::Eof, "".to_owned(), None, self.line));
     }
 
     // note: not important but possibly somewhere to use #[inline]
-    fn isAtEnd(&self) -> bool {
+    fn is_at_end(&self) -> bool {
         self.current as usize >= self.source.len()
     }
-    
-    fn scanToken(&mut self) -> () {
+
+    fn scan_token(&mut self) -> () {
         let c = self.advance();
 
         match c {
-            '(' => self.tokens.push(self.addToken(TokenType::LeftParen, None)),
-            ')' => self.tokens.push(self.addToken(TokenType::RightParen, None)),
-            '{' => self.tokens.push(self.addToken(TokenType::LeftBrace, None)),
-            '}' => self.tokens.push(self.addToken(TokenType::RightBrace, None)),
-            ',' => self.tokens.push(self.addToken(TokenType::Comma, None)),
-            '.' => self.tokens.push(self.addToken(TokenType::Dot, None)),
-            '-' => self.tokens.push(self.addToken(TokenType::Minus, None)),
-            '+' => self.tokens.push(self.addToken(TokenType::Plus, None)),
-            ';' => self.tokens.push(self.addToken(TokenType::Semicolon, None)),
-            '*' => self.tokens.push(self.addToken(TokenType::Star, None)),
-            _ => report(self.line, self.start, &self.source[self.start as usize..self.current as usize], "Unexpected character."),
+            '(' => self.add_token(TokenType::LeftParen, None),
+            ')' => self.add_token(TokenType::RightParen, None),
+            '{' => self.add_token(TokenType::LeftBrace, None),
+            '}' => self.add_token(TokenType::RightBrace, None),
+            ',' => self.add_token(TokenType::Comma, None),
+            '.' => self.add_token(TokenType::Dot, None),
+            '-' => self.add_token(TokenType::Minus, None),
+            '+' => self.add_token(TokenType::Plus, None),
+            ';' => self.add_token(TokenType::Semicolon, None),
+            '*' => self.add_token(TokenType::Star, None),
+            '!' => self.add_token(
+                if self.match_token('=') {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                },
+                None,
+            ),
+            '=' => self.add_token(
+                if self.match_token('=') {
+                    TokenType::EqualEqual
+                } else {
+                    TokenType::Equal
+                },
+                None,
+            ),
+            '>' => self.add_token(
+                if self.match_token('=') {
+                    TokenType::GreaterEqual
+                } else {
+                    TokenType::Greater
+                },
+                None,
+            ),
+            '<' => self.add_token(
+                if self.match_token('=') {
+                    TokenType::LessEqual
+                } else {
+                    TokenType::Less
+                },
+                None,
+            ),
+            '/' => {
+                if self.match_token('/') {
+                    while !self.is_at_end() && !(self.peek() == '\n') {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash, None);
+                }
+            }
+            '\n' => self.line += 1,
+            '\t' => (),
+            ' ' => (),
+            '\r' => (),
+            '"' => self.string(),
+            _ => (self.report)(
+                self.line,
+                self.start,
+                &self.source[self.start as usize..self.current as usize],
+                "Unexpected character.",
+            ),
         };
     }
-    
+
+    fn match_token(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            false
+        } else if self.peek() != expected {
+            false
+        } else {
+            self.current += 1;
+            true
+        }
+    }
+
     // TODO: horrible, figure out how to use an iterator instead
     fn advance(&mut self) -> char {
         let c = self.source.as_bytes()[self.current as usize];
         self.current += 1;
         c as char
     }
-    
-    fn addToken(&self, token_type: TokenType, literal: Option<Literal>) -> Token {
+
+    fn add_token(&mut self, token_type: TokenType, literal: Option<Literal>) -> () {
         // note: will panic if start..current doesn't encompass a valid character sequence
         let text = &self.source[self.start as usize..self.current as usize]; // no off by one, advance() increments current by 1
-        Token::new(token_type, text.to_owned(), literal, self.line)
+        let token = Token::new(token_type, text.to_owned(), literal, self.line);
+        self.tokens.push(token);
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+        self.source.as_bytes()[self.current as usize] as char
+    }
+
+    fn string(&mut self) -> () {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            (self.report)(self.line, self.start, &self.source[self.start as usize..self.current as usize], "");
+        }
     }
 }
-
-
