65f9a564ac53d586887bf97721fbab8dd873dec3
Ali Shirazi <afashirazi@proton.me>
2025-07-18 13:57:10 +0300

    enclosing

--- p1/src/environment.rs
+++ p1/src/environment.rs
@@ -3,12 +3,21 @@ use std::collections::HashMap;
 use crate::expr::Literal;
 
 pub struct Environment {
+    enclosing: Option<Box<Environment>>, // reference to parent environment, for scoping
     map: HashMap<String, Literal>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Self {
+            enclosing: None,
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn with_enclosing(parent: Environment) -> Self {
+        Self {
+            enclosing: Some(Box::new(parent)),
             map: HashMap::new(),
         }
     }
